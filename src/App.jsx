d6432018372851410bba32cb82d2a4f3e14f6ca6
Natalia Sargarovschi <natalia.sargarovschi@amdaris.com>
2023-09-18 19:09:24 +0300

    creating Directory and Category item component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Directory from './components/category-item/directory/directory-component';
+import './categories.styles.scss';
+
+const App = () => {
+  return <Directory />;
+};
+
+export default App;
