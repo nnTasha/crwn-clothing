66f9ed3bc47253c15f6e9d3cd7906d68736539bd
Natalia Sargarovschi <natalia.sargarovschi@amdaris.com>
2023-09-25 16:37:27 +0300

    refactor Directory and DirectoryItem component

--- src/components/directory-item/directory-item.jsx
+++ src/components/directory-item/directory-item.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import {
   BackgroundImage,
   Body,
@@ -5,9 +6,13 @@ import {
 } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const navigationHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={navigationHandler}>
       <BackgroundImage image={imageUrl} />
       <Body>
         <h2>{title}</h2>
